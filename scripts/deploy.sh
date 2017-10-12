cbc7b1c841319fac26ad58506938a38d314915a3
laurasj <lauras@zenitech.co.uk>
2017-10-12 11:46:19 +0100

    v12

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+eval $(aws ecr get-login --no-include-email | sed 's|https://||')
 /opt/ecs-deploy/ecs-deploy -c laurastestgo -n laurogoservisas -i 604459335658.dkr.ecr.eu-west-2.amazonaws.com/laurastestgo:latest
